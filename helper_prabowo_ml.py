b3b9d33f0425231337e5479db9dadfd89ab11411
Prabowo Yoga Wicaksana <62101089+yogawicaksana@users.noreply.github.com>
2022-07-07 12:40:14 +0700

    Update helper_prabowo_ml.py
    add text_preprocessing

--- helper_prabowo_ml.py
+++ helper_prabowo_ml.py
@@ -292,3 +292,80 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+
+# =========================TEXT PREPROCESSING===========================
+# remove hashtags
+def hashtags(text):
+  hash = re.findall(r"#(\w+)", text)
+  return hash
+
+# translate emoji
+def emoji(text):
+  for emot in UNICODE_EMOJI:
+    if text == None:
+      text = text
+    else:
+      text = text.replace(emot, "_".join(UNICODE_EMOJI[emot].replace(",", "").replace(":", "").split()))
+    return text
+
+# remove retweet username and tweeted at @username
+def remove_users(tweet):
+  '''Takes a string and removes retweet and @user information'''
+  tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet) 
+  # remove tweeted at
+  return tweet
+
+# remove links
+def remove_links(tweet):
+  '''Takes a string and removes web links from it'''
+  tweet = re.sub(r'http\S+', '', tweet) # remove http links
+  tweet = re.sub(r'bit.ly/\S+', '', tweet) # remove bitly links
+  tweet = tweet.strip('[link]') # remove [links]
+  return tweet
+def clean_html(text):
+  html = re.compile('<.*?>')#regex
+  return html.sub(r'',text)
+
+# remove non ascii character
+def non_ascii(s):
+  return "".join(i for i in s if ord(i)<128)
+
+def lower(text):
+  return text.lower()
+
+# remove stopwords
+def removeStopWords(str):
+#select english stopwords
+  cachedStopWords = set(stopwords.words("english"))
+#add custom words
+  cachedStopWords.update(('and','I','A','http','And','So','arnt','This','When','It','many','Many','so','cant','Yes','yes','No','no','These','these','mailto','regards','ayanna','like','email'))
+#remove stop words
+  new_str = ' '.join([word for word in str.split() if word not in cachedStopWords]) 
+  return new_str
+
+# remove email address
+def email_address(text):
+  email = re.compile(r'[\w\.-]+@[\w\.-]+')
+  return email.sub(r'',text)
+
+def punct(text):
+  token=RegexpTokenizer(r'\w+')#regex
+  text = token.tokenize(text)
+  text= " ".join(text)
+  return text 
+
+# remove digits and special characters
+def remove_digits(text):
+    pattern = r'[^a-zA-z.,!?/:;\"\'\s]' 
+    #when the ^ is on the inside of []; we are matching any character that is not included in this expression within the []
+    return re.sub(pattern, '', text)
+
+def remove_special_characters(text):
+    # define the pattern to keep
+    pat = r'[^a-zA-z0-9.,!?/:;\"\'\s]' 
+    return re.sub(pat, '', text)
+
+def remove_(tweet):
+  tweet = re.sub('([_]+)', "", tweet)
+  return tweet
